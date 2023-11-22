4e0849c69bb210e8ebc87cddcca87141b23890ed
nithindaniel1 <nithindaniel2018@gmail.com>
2023-11-22 15:07:27 +0530

    get all post api

--- api/views.py
+++ api/views.py
@@ -1,11 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Blog
 # Create your views here.
 class Sample(APIView):
 
      def get(self, request, format=None):
-        """
-        Return a list of all users.
-        """
-        usernames = "Sample Api for testing"
-        return Response(usernames)
+        get_post = Blog.objects.all()
+        return Response(get_post)
